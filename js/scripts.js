91f4b1ef3a19baeaa99464f7fcaa909b3e75e78f
Gerardo Merida <cgemerida@gmail.com>
2019-06-15 21:03:31 -0600

    NPM Scripts part 2

--- js/scripts.js
+++ js/scripts.js
@@ -9,15 +9,15 @@ $(document).ready(function () {
     // });
 
     $('#carousel-btn').click(function () {
-        let value = $(this).hasClass('btn-danger');
+        var value = $(this).hasClass('btn-danger');
         if (value) {
             $('#mycarousel').carousel('pause');
             $(this).removeClass('btn-danger').addClass('btn-success');
-            $(this).empty().html(`<i class="fa fa-play"></i>`);
+            $(this).empty().html('<i class="fa fa-play"></i>');
         } else {
             $('#mycarousel').carousel('cycle');
             $(this).removeClass('btn-success').addClass('btn-danger');
-            $(this).empty().html(`<i class="fa fa-pause"></i>`);
+            $(this).empty().html('<i class="fa fa-pause"></i>');
         }
     });
 
